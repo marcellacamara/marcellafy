30418e0651bdf2a58884877f44ee961755bcd1b6
Marcella Câmara <marcella-camara@live.com>
2022-05-10 15:31:29 -0300

    front (dashboard quase finalizado)

--- resources/views/site/dashboard-admin.blade.php
+++ resources/views/site/dashboard-admin.blade.php
@@ -1,8 +1,8 @@
 <x-layout.admin>
 
-  <div class="grid grid-cols-3">
+  <div class="p-4 grid grid-cols-2 gap-4">
     @foreach ($artists as $artist)
-      <x-layout.card :artist="$artist" />
+      <x-layout.card :artist="$artist" :href="route('admin.artists.albums.index', $artist->id)" />
     @endforeach
   </div>
 
